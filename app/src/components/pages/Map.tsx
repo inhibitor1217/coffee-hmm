6f53fd54d10700f0a664aa3f9e06c400a196cd8f
Jynn Park <yuunnii315@gmail.com>
2020-08-06 14:57:07 +0900

    add markers

--- app/src/components/pages/Map.tsx
+++ app/src/components/pages/Map.tsx
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Styled from 'styled-components';
 import './Map.css';
+import { listenerCount } from 'stream';
 
 declare global{
     interface Window{
       kakao: any;
     }
   }
-
-const StyledButton = Styled.button`
-    color: gray;
-`
-const cafeList = [
-    {name: 'Water-White', location:[37.401251, 127.110753]},
-    {name: 'Cafe-Moire', location: [37.401399, 127.110903]}
-];
 
 const Map = () => {
     useEffect(() =>{
@@ -25,6 +18,32 @@ const Map = () => {
         };
     
         let map = new window.kakao.maps.Map(container, options);
+
+        let cafeList = [
+            {
+                title: 'Water-White', 
+                latlng: new window.kakao.maps.LatLng(37.401251, 127.110753)
+            },
+            {
+                title: 'Cafe-Moire', 
+                latlng: new window.kakao.maps.LatLng(37.401399, 127.110903)
+            }
+        ];
+
+        let markerImgSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+        for(let i=0; i<cafeList.length; i++){
+            let markerSize = new window.kakao.maps.Size(24,35);
+            let markerImg = new window.kakao.maps.MarkerImage(markerImgSrc, markerSize);
+
+            let marker = new window.kakao.maps.Marker({
+                map: map,
+                position: cafeList[i].latlng,
+                title: cafeList[i].title,
+                image: markerImg
+            });
+        }
+
       }, [])
 
     return (
