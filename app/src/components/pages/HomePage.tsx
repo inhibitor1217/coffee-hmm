d46acb595dad5a461ce21429db1f03a1db0e6f11
Jynn Park <yuunnii315@gmail.com>
2020-08-06 14:27:52 +0900

    change map api

--- app/src/components/pages/HomePage.tsx
+++ app/src/components/pages/HomePage.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
+import Map from './Map';
+import MaterialIcon from "../common/MaterialIcon";
+
+
 
 const HomePage = () => {
-  return <div>Welcome to coffee-hmm!</div>;
+  return (
+  <div>Welcome to coffee-hmm!
+    <Map />
+  </div>
+    );
 };
 
 export default HomePage;
--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
