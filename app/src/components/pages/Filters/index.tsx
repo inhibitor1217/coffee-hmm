2ff18607d0e5508c2478e990ef25028cc4e08006
Jynn Park <yuunnii315@gmail.com>
2020-08-09 17:07:05 +0900

    add filter2

---
+++ app/src/components/pages/Filters/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./index.css";
+import { Cafe } from "../Cafes";
+
+interface FilterProps {
+  setFilter(filter: () => (cafe: Cafe) => boolean): void;
+}
+
+const Filter = (props: FilterProps) => {
+  return (
+    <div id="footer">
+      <button
+        className="filters"
+        onClick={() => props.setFilter(() => (cafe) => cafe.avgPrice >= 3000)}
+      >
+        PRICE
+      </button>
+      <button
+        className="filters"
+        onClick={() => props.setFilter(() => (cafe) => cafe.isVisited)}
+      >
+        VISITED
+      </button>
+      <button className="filters">filter 3</button>
+    </div>
+  );
+};
+
+export default Filter;
