b3e40b38bbba1ab84c31b7a37ec135c845f7ae84
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-08-09 14:33:08 +0900

    implement page transition using react-spring

--- app/src/components/pages/HomePage.tsx
+++ app/src/components/pages/HomePage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>Welcome to coffee-hmm!</div>;
+  return (
+    <div>
+      Welcome to coffee-hmm!
+      <Link to="/cafe/1">
+        <button>link to page</button>
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
