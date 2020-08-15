798f892287add332eae0b6f42f98513386a9add4
Jynn Park <yuunnii315@gmail.com>
2020-08-15 12:45:36 +0900

    add street line

---
+++ app/src/components/pages/Street/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.css";
+import { CafeInfo } from "../Cafes";
+
+const Street = () => {
+  return <div className="street"></div>;
+};
+
+export default Street;
