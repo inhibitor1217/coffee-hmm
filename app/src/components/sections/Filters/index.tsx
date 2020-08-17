a1507b077b0bf284a72a5e98d56ad8db8f33713e
Jynn Park <yuunnii315@gmail.com>
2020-08-17 09:59:21 +0900

    change overall structure

--- app/src/components/sections/Filters/index.tsx
+++ app/src/components/sections/Filters/index.tsx
@@ -7,24 +7,22 @@ interface FilterProps {
 }
 
 const Filter = (props: FilterProps) => {
+  const btnUri = props ? `url(images/green.png)` : undefined;
   return (
     <div id="filters">
       <button
-        className="btn"
+        className="btn btn-price"
+        style={{
+          backgroundImage: btnUri,
+        }}
         onClick={() => props.setFilter(() => (cafe) => cafe.avgPrice >= 3000)}
       >
-        {/* <a href="#" className="filter_btn">
-          price
-        </a> */}
         price
       </button>
       <button
         className="btn"
         onClick={() => props.setFilter(() => (cafe) => cafe.isVisited)}
       >
-        {/* <a href="#" className="filter_btn">
-          history
-        </a> */}
         history
       </button>
     </div>
