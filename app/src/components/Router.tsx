00257c394dd464515e3adc0396d3e3be9633a8ac
Jynn Park <yuunnii315@gmail.com>
2020-08-18 14:21:39 +0900

    add cafe-info route

--- app/src/components/Router.tsx
+++ app/src/components/Router.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { useLocation } from "react-router";
 import { Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "./pages/Homepage";
+import InfoPage from "./pages/Infopage";
 
 const Router = () => {
   const location = useLocation();
   return (
     <Switch location={location}>
       <Route path="/" exact render={() => <HomePage />} />
+      <Route pathe="/cafe" component={InfoPage} />
       <Redirect to="/" />
     </Switch>
   );
