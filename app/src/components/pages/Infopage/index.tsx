6a5a5b424e1442c990e7474d7b569d1011e4f841
Jynn Park <yuunnii315@gmail.com>
2020-08-18 14:21:39 +0900

    add cafe-info route

---
+++ app/src/components/pages/Infopage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CafeInformation from "../../sections/CafeInformation";
+
+const InfoPage = () => {
+  return (
+    <div>
+      <CafeInformation />
+    </div>
+  );
+};
+
+export default InfoPage;
---
+++ app/src/components/sections/CafeInformation/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CafeInformation = () => {
+  return <div>testing...</div>;
+};
+
+export default CafeInformation;
