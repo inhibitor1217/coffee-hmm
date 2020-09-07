9688b07bd0e89e3d5e2662111ca9f182637b0e05
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-09-07 15:18:18 +0900

    add populating cafe data with meus

--- server/src/client.js
+++ server/src/client.js
@@ -1,10 +1,12 @@
 const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
 const uuid = require('uuid');
+const fs = require('fs');
 
 dotenv.config('.env');
 
 const CAFES_TABLE = 'coffee-hmm-server-cafes-production';
+const MENUS_TABLE = 'coffee-hmm-server-menus-production';
 const RESOURCE_BUCKET_URI = 'coffee-hmm-resource.inhibitor.io';
 
 AWS.config.update({
@@ -80,4 +82,68 @@ function deleteItem(client, id) {
   });
 }
 
-scanCafes(documentClient);
+function createMenu(client, menu) {
+  const params = {
+    TableName: MENUS_TABLE,
+    Item: menu
+  };
+  
+  client.put(params, (err, data) => {});
+}
+
+function populateCafe(client, cafe, callback) {
+  const { name } = cafe;
+  if (!name) return cafe;
+
+  client.scan({
+    TableName: MENUS_TABLE,
+    ExpressionAttributeValues: {
+      ':c': name
+    },
+    FilterExpression: 'cafeName = :c'
+  }, function(err, data) {
+    if (err) {
+      if (callback) callback(err);
+      return;
+    }
+
+    const menus = { categories: [] };
+    data.Items.forEach((menu) => {
+      const { cafeName, categoryName, ...rest } = menu;
+      const idx = menus.categories.findIndex((category) => category.categoryName === categoryName);
+      if (idx >= 0) {
+        menus.categories[idx].categoryMenu = [
+          ...menus.categories[idx].categoryMenu,
+          rest
+        ];
+      } else {
+        menus.categories = [...menus.categories, {
+          categoryName,
+          categoryMenu: [rest]
+        }];
+      }
+    });
+
+    const { price: americanoPrice = undefined } = data.Items.find((menu) => menu.ename === 'Americano') || {};
+
+    if (callback) callback(null, { ...cafe, menus, americanoPrice });
+  });
+}
+
+function scanCafes(client) {
+  client.scan({
+    TableName: CAFES_TABLE
+  }, function(err, data) {
+    if (err) {
+      console.error(JSON.stringify(err, null, 2));
+      return;
+    }
+
+    data.Items.forEach((cafe) => populateCafe(client, cafe, (err, data) => {
+      console.log(JSON.stringify(data, null, 2));
+    }));
+  })
+}
+
+// scanCafes(documentClient);
+getCafeItem(documentClient, "40e565e9-8c80-4eee-94c1-f5d164a13e74");
