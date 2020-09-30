265797b8b913640a18c0919c0a89af186ae432ed
inhibitor <inhibitor@kaist.ac.kr>
2020-09-30 15:02:03 +0900

    apply code splitting according to each route

--- app/src/components/Router.tsx
+++ app/src/components/Router.tsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import MainPage from "./pages/Main";
-import CafePlacePage from "./pages/Place";
-import CafeDetailPage from "./pages/CafeDetail";
-import PlaceListPage from "./pages/PlaceList";
 
 interface RouterProps {
   location?: any;
 }
 
+const MainPage = lazy(() => import("./pages/Main"));
+const CafeDetailPage = lazy(() => import("./pages/CafeDetail"));
+const CafePlacePage = lazy(() => import("./pages/Place"));
+const PlaceListPage = lazy(() => import("./pages/PlaceList"));
+
 const Router = (props: RouterProps) => {
   return (
-    <Switch location={props.location}>
-      <Route path="/" exact render={() => <MainPage />} />
-      <Route path="/cafe/:cafeId" render={() => <CafeDetailPage />} />
-      <Route path="/place/:place" render={() => <CafePlacePage />} />
-      <Route path="/places" render={() => <PlaceListPage />} />
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Switch location={props.location}>
+        <Route path="/" exact render={() => <MainPage />} />
+        <Route path="/cafe/:cafeId" render={() => <CafeDetailPage />} />
+        <Route path="/place/:place" render={() => <CafePlacePage />} />
+        <Route path="/places" render={() => <PlaceListPage />} />
 
-      <Redirect to="/" />
-    </Switch>
+        <Redirect to="/" />
+      </Switch>
+    </React.Suspense>
   );
 };
 
