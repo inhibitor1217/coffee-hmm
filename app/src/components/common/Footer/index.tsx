bd6acbbd8c3d0daa0fe6556106ee811822ad9ed2
Jynn Park <yuunnii315@gmail.com>
2020-11-19 02:48:43 +0900

    add missing files from coffee-hmm app version1.0

--- app/src/components/common/Footer/index.tsx
+++ app/src/components/common/Footer/index.tsx
@@ -13,7 +13,7 @@ const Footer = ({location}: FooterProps) => {
     return(
         <footer>
             <Link to="/" style={{color: pathname === "/" ? "#524F4A" :  "#dbdbdb"}}><i className="material-icons">home</i><span>Home</span></Link>
-            <Link to="/search" style={{color: pathname === "/search" ? "#524F4A" :  "#dbdbdb"}}><i className="material-icons-round">search</i><span>Search</span></Link>
+            <Link to="/" style={{color: pathname === "/search" ? "#524F4A" :  "#dbdbdb"}}><i className="material-icons-round">search</i><span>Search</span></Link>
             <Link to="/signin" style={{color: pathname === "/signin" ? "#524F4A" :  "#dbdbdb"}}><i className="material-icons">person</i><span>My</span></Link>
         </footer>
     )
