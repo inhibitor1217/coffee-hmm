60d36a33de05084b3c57d592d0ac69a72bf1a2cf
inhibitor <inhibitor@kaist.ac.kr>
2020-12-14 21:26:27 +0900

    feat: implement jwt generation and verification

---
+++ services/auth/src/util/token.ts
@@ -0,0 +1,56 @@
+import { sign, SignOptions, verify } from 'jsonwebtoken';
+import { appStage, env } from '.';
+import service from '../services';
+import { AppStage } from '../types/env';
+
+const issuer = (): string => `${env('APP_STAGE')}.auth.coffee-hmm.inhibitor.io`;
+const secretKey = async (): Promise<string> =>
+  appStage() === AppStage.local
+    ? env('JWT_SECRET_KEY')
+    : service().secret().get('JWT_SECRET_KEY');
+
+export const generateToken = async <T extends Record<string, unknown>>(
+  payload: T,
+  options?: SignOptions
+): Promise<string> => {
+  const jwtSecretKey = await secretKey();
+  return new Promise((resolve, reject) => {
+    sign(
+      payload,
+      jwtSecretKey,
+      {
+        ...options,
+        issuer: issuer(),
+      },
+      (err, token) => {
+        if (err || !token) {
+          reject(err);
+        } else {
+          resolve(token);
+        }
+      }
+    );
+  });
+};
+
+export const verifyToken = async <T extends Record<string, unknown>>(
+  token: string
+): Promise<T> => {
+  const jwtSecretKey = await secretKey();
+  return new Promise<T>((resolve, reject) => {
+    verify(
+      token,
+      jwtSecretKey,
+      {
+        issuer: issuer(),
+      },
+      (err, payload) => {
+        if (err || !payload) {
+          reject(err);
+        } else {
+          resolve(payload as T);
+        }
+      }
+    );
+  });
+};
