ca07cbf402e55d50c282d35584df34ded263f5a5
inhibitor <inhibitor@kaist.ac.kr>
2020-12-20 18:51:16 +0900

    fix: do not reference entity files as regex on ormconfig

--- services/auth/src/middlewares/db.ts
+++ services/auth/src/middlewares/db.ts
@@ -1,12 +1,15 @@
 import 'pg';
 
+import { readFile } from 'fs';
 import { Middleware, Next, ParameterizedContext } from 'koa';
-import { Connection, createConnection } from 'typeorm';
+import path from 'path';
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 import { createPolicyLoader } from '../entities/policy';
 import { createUserLoader, createUserProfileLoader } from '../entities/user';
 import { AppStage } from '../types/env';
 import { KoaContextState } from '../types/koa';
 import { appStage } from '../util';
+import entities from '../entities';
 
 const createDataLoaders = (context: KoaContextState) => ({
   user: createUserLoader(context),
@@ -16,13 +19,32 @@ const createDataLoaders = (context: KoaContextState) => ({
 
 export type DataLoaders = ReturnType<typeof createDataLoaders>;
 
+const readOrmConfig = () =>
+  new Promise<ConnectionOptions>((resolve, reject) => {
+    readFile(
+      path.resolve(process.cwd(), 'ormconfig.json'),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const config = JSON.parse(data);
+          resolve((config as unknown) as ConnectionOptions);
+        }
+      }
+    );
+  });
+
 const db = (): Middleware<KoaContextState> => {
   let connection: Connection | null = null;
 
   return async (ctx: ParameterizedContext<KoaContextState>, next: Next) => {
     ctx.state.connection = async (): Promise<Connection> => {
       if (!connection) {
-        connection = await createConnection();
+        connection = await createConnection({
+          ...(await readOrmConfig()),
+          entities,
+        });
       }
       return connection;
     };
