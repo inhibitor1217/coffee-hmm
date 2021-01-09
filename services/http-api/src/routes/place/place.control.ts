b3b8889839eb00b7ec2fff4022d5120f9e3ccd41
inhibitor <inhibitor@kaist.ac.kr>
2021-01-09 16:07:44 +0900

    feat(api): implement GET /place/list

--- services/http-api/src/routes/place/place.control.ts
+++ services/http-api/src/routes/place/place.control.ts
@@ -1,8 +1,25 @@
+import { getRepository } from 'typeorm';
+import { HTTP_OK } from '../../const';
+import Place from '../../entities/place';
+import { KoaRouteHandler } from '../../types/koa';
 import Exception, { ExceptionCode } from '../../util/error';
 import handler from '../handler';
 
-export const getList = handler(() => {
-  throw new Exception(ExceptionCode.notImplemented);
+export const getList: KoaRouteHandler = handler(async (ctx) => {
+  await ctx.state.connection();
+
+  const places = await getRepository(Place)
+    .createQueryBuilder('place')
+    .select()
+    .getMany();
+
+  ctx.status = HTTP_OK;
+  ctx.body = {
+    place: {
+      count: places.length,
+      list: places.map((place) => place.toJsonObject()),
+    },
+  };
 });
 
 export const create = handler(() => {
