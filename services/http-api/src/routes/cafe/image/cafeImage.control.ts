72c0dc04d396aefd1bb93b20c113f1733a1b8713
inhibitor <inhibitor@kaist.ac.kr>
2021-01-12 16:02:24 +0900

    feat(api): implement PUT /cafe/:cafeId/image/:cafeImageId

--- services/http-api/src/routes/cafe/image/cafeImage.control.ts
+++ services/http-api/src/routes/cafe/image/cafeImage.control.ts
@@ -1,7 +1,7 @@
 import joi from 'joi';
 import { FOREIGN_KEY_VIOLATION } from 'pg-error-constants';
-import { getManager } from 'typeorm';
-import { HTTP_CREATED } from '../../../const';
+import { EntityManager, getManager } from 'typeorm';
+import { HTTP_CREATED, HTTP_OK } from '../../../const';
 import CafeImage, {
   CafeImageState,
   CafeImageStateStrings,
@@ -108,13 +108,180 @@ export const create: KoaRouteHandler<
   }
 );
 
+const checkConsistency = async (manager: EntityManager, cafeId: string) => {
+  const images = await manager
+    .createQueryBuilder(CafeImage, 'cafe_image')
+    .select()
+    .where(`cafe_image.fk_cafe_id = :cafeId`, { cafeId })
+    .andWhere(`cafe_image.state IS DISTINCT FROM :deleted`, {
+      deleted: CafeImageState.deleted,
+    })
+    .orderBy(`cafe_image.index`, 'ASC')
+    .getMany();
+
+  /* exactly one among the active images should be main image. */
+  const activeImages = images.filter(
+    (image) => image.state === CafeImageState.active
+  );
+  const hiddenImages = images.filter(
+    (image) => image.state === CafeImageState.hidden
+  );
+
+  if (hiddenImages.some((image) => image.isMain)) {
+    throw new Exception(
+      ExceptionCode.badRequest,
+      `inconsistent state: hidden image is set as main image`
+    );
+  }
+
+  if (activeImages.length > 0) {
+    const numMainImages = activeImages.reduce(
+      (acc, cur) => acc + (cur.isMain ? 1 : 0),
+      0
+    );
+
+    if (numMainImages > 1) {
+      throw new Exception(
+        ExceptionCode.badRequest,
+        `inconsistent state: there are more than one main image`
+      );
+    }
+    if (numMainImages < 1) {
+      throw new Exception(
+        ExceptionCode.badRequest,
+        `inconsistent state: there is no main image`
+      );
+    }
+  }
+
+  /* the indices should be correctly labeled */
+  images.forEach((image, index) => {
+    if (image.index !== index) {
+      throw new Exception(
+        ExceptionCode.badRequest,
+        `inconsistent state: wrong indices applied to images`
+      );
+    }
+  });
+};
+
 export const updateList = handler(() => {
   throw new Exception(ExceptionCode.notImplemented);
 });
 
-export const updateOne = handler(() => {
-  throw new Exception(ExceptionCode.notImplemented);
-});
+export const updateOne: KoaRouteHandler<
+  {
+    cafeId: string;
+    cafeImageId: string;
+  },
+  VariablesMap,
+  {
+    uri?: string;
+    metadata?: AnyJson | null;
+    state?: CafeImageStateStrings;
+  }
+> = handler(
+  async (ctx) => {
+    if (!ctx.request.body) {
+      throw new Exception(ExceptionCode.badRequest);
+    }
+
+    const { cafeId, cafeImageId } = ctx.params;
+    const { uri, metadata, state: stateString } = ctx.request.body;
+    const state = stateString ? CafeImageState[stateString] : stateString;
+
+    const connection = await ctx.state.connection();
+
+    const updated = await connection.transaction(async (manager) => {
+      const cafeImages = await manager
+        .createQueryBuilder(CafeImage, 'cafe_image')
+        .select()
+        .where(`cafe_image.fk_cafe_id = :cafeId`, { cafeId })
+        .andWhere(`cafe_image.state IS DISTINCT FROM :deleted`, {
+          deleted: CafeImageState.deleted,
+        })
+        .orderBy(`cafe_image.index`, 'ASC')
+        .getMany();
+
+      const originalCafeImage = cafeImages.find(
+        (cafeImage) => cafeImage.id === cafeImageId
+      );
+      const shouldUpdateAsMain =
+        originalCafeImage?.state === CafeImageState.hidden &&
+        state === CafeImageState.active &&
+        !cafeImages.reduce((acc, image) => acc || image.isMain, false);
+
+      const updatedCafeImage = await manager
+        .createQueryBuilder()
+        .update(CafeImage)
+        .set(
+          Object.filterUndefinedKeys({
+            relativeUri: uri,
+            metadata: metadata ? JSON.stringify(metadata) : metadata,
+            state,
+            isMain: shouldUpdateAsMain ? true : undefined,
+          })
+        )
+        .where(`cafe_images.id = :cafeImageId`, { cafeImageId })
+        .andWhere(`cafe_images.fk_cafe_id = :cafeId`, { cafeId })
+        .andWhere(`cafe_images.state IS DISTINCT FROM :deleted`, {
+          deleted: CafeImageState.deleted,
+        })
+        .returning(CafeImage.columns)
+        .execute()
+        .then((updateResult) => {
+          if (!updateResult.affected) {
+            throw new Exception(ExceptionCode.notFound);
+          }
+
+          return CafeImage.fromRawColumns(
+            (updateResult.raw as Record<string, unknown>[])[0]
+          );
+        });
+
+      await checkConsistency(manager, cafeId);
+
+      return updatedCafeImage;
+    });
+
+    ctx.status = HTTP_OK;
+    ctx.body = {
+      cafe: {
+        id: cafeId,
+        image: updated?.toJsonObject(),
+      },
+    };
+  },
+  {
+    schema: {
+      params: joi
+        .object()
+        .keys({
+          cafeId: joi.string().uuid({ version: 'uuidv4' }).required(),
+          cafeImageId: joi.string().uuid({ version: 'uuidv4' }).required(),
+        })
+        .required(),
+      body: joi
+        .object()
+        .keys({
+          uri: joi.string().min(1).max(255),
+          metadata: joi.object().allow(null),
+          state: joi
+            .string()
+            .valid(...enumKeyStrings(CafeImageState))
+            .disallow(CafeImageState[CafeImageState.deleted]),
+        })
+        .or('uri', 'metadata', 'state')
+        .required(),
+    },
+    requiredRules: (ctx) =>
+      new OperationSchema({
+        operationType: OperationType.mutation,
+        operation: 'api.cafe.image',
+        resource: ctx.params.cafeImageId,
+      }),
+  }
+);
 
 export const deleteList = handler(() => {
   throw new Exception(ExceptionCode.notImplemented);
