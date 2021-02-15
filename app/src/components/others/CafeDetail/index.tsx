ba4354d094cedc529683bf8b80410b7fe17e6259
jyuunnii <yuunnii315@gmail.com>
2021-02-15 21:54:39 +0900

    feat: v2.1 업데이트 - 헤더 배경색 추가 - 디테일 페이지 불필요한 기능 제거: 리뷰, 사진업로드 - 디테일 페이지 운영시간 수정

--- app/src/components/others/CafeDetail/index.tsx
+++ app/src/components/others/CafeDetail/index.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { useHistory } from 'react-router-dom';
-import { copyLink} from '../../../utils/function';
+import { copyLink, openSearch} from '../../../utils/function';
 import { StyledColumnFlex, StyledRowFlexCenter } from '../../../utils/styled';
 import { TypeCafe } from '../../../utils/type';
 import CafeDetailImageCarousel from '../CafeDetailImageCarousel';
-import WebSearchBottomPopup from '../WebSearchBottomPopup';
 import './index.css';
 
 type CafeDetailProps = {
@@ -14,7 +13,6 @@ type CafeDetailProps = {
 }
 
 const CafeDetail = ({ cafe }: CafeDetailProps) => {
-    const [isWebSearchClicked, setWebSearchClicked] = useState<boolean>(false);
     const currentCopyLink = `https://coffee-hmm.inhibitor.io/cafe/${cafe?.id}`;
     const history = useHistory();
     
@@ -31,22 +29,15 @@ const CafeDetail = ({ cafe }: CafeDetailProps) => {
                 <div className="detail-info">
                     <span className="detail-name">{cafe?.name}</span>
                     <span className="detail-place">{cafe?.place.name}</span>
-                    <span className="detail-time">open 9:00 ~ 19:00</span>
+                    <span className="detail-time">OPEN {cafe?.metadata?.hour}</span>
                 </div>
 
                 <StyledRowFlexCenter className="detail-button-wrapper">
                     <div className="detail-button"><button className="detail-like"><i className="material-icons-round">favorite</i></button><span>좋아요</span></div>
-                    <div className="detail-button"> <CopyToClipboard text={currentCopyLink}><button className="detail-share" onClick={() => copyLink(cafe?.name)}><i className="material-icons-round">share</i></button></CopyToClipboard><span>링크 공유</span></div>
-                    <div className="detail-button"><button className="detail-review"><i className="material-icons-round">create</i></button><span>카페 리뷰</span></div>
-                    <div className="detail-button"><button className="detail-photo"><i className="material-icons-round">photo_library</i></button><span>사진 업로드</span></div>  
+                    <div className="detail-button"> <CopyToClipboard text={currentCopyLink}><button className="detail-share" onClick={() => copyLink(cafe?.name)}><i className="material-icons-round">share</i></button></CopyToClipboard><span>흠 링크<br/>공유하기</span></div>
+                    <div className="detail-button" onClick={() => openSearch((cafe?.name || "")+" "+cafe?.place.name, "Naver")}><button className="detail-naver"><img src="/images/naver-icon.png" alt="naver"/></button><span>네이버 검색<br/>바로가기</span></div>
+                    <div className="detail-button" onClick={() => openSearch((cafe?.name || ""), "Instagram")}><button className="detail-insta"><img src="/images/insta-icon.png" alt="insta"/></button><span>인스타그램<br/>바로가기</span></div>  
                 </StyledRowFlexCenter>
-
-                <div className="detail-more">
-                    <h5>카페의 구체적인 정보가 필요하신가요&#63;</h5>
-                    <div className="detail-more-button"><button onClick={() => setWebSearchClicked(true)}>네이버, 인스타 검색 바로가기 &#10154;</button></div>
-                    <div className="detail-map-button"><button>지도 바로가기 &#10154;</button></div>
-                    <WebSearchBottomPopup cafe={cafe} isWebSearchClicked={isWebSearchClicked} setWebSearchClicked={setWebSearchClicked}/>
-                </div>
             </StyledColumnFlex>
         </div>
     )
