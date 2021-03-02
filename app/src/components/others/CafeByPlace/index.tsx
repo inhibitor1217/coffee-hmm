fff6b6a4e19f01c439f4b22561aad433912b0b74
inhibitor <inhibitor@kaist.ac.kr>
2021-03-02 15:52:04 +0900

    refactor: rewrite CafeByPlace using SwipeablePanel

--- app/src/components/others/CafeByPlace/index.tsx
+++ app/src/components/others/CafeByPlace/index.tsx
@@ -1,37 +1,67 @@
-import React, { useEffect, useRef } from 'react';
+import { SwipeablePanel, Axis } from '@inhibitor1217/react-swipeablepanel';
+import React, { useEffect, useMemo, useState } from 'react';
+import { openSearch } from '../../../utils/function';
 import { TypeCafe } from '../../../utils/type';
-import { StyledCarouselImage } from '../../../utils/styled';
+import { StyledCarouselImage, StyledRowFlex } from '../../../utils/styled';
 import CarouselMainImage from '../../others/CarouselMainImage';
-import CarouselHorizontal from '../../others/CarouselHorizontal';
+import './index.css';
 
 type CafeByPlaceProps = {
     cafes: TypeCafe[];
-    setCurrentCafeIndex: (index: number) => void;
 }
 
-const CafeByPlace = ({cafes, setCurrentCafeIndex}: CafeByPlaceProps) => {
-    const ref = useRef<number>(0);
+const CafeByPlace = ({ cafes }: CafeByPlaceProps) => {
+  const [cafeIndex, setCafeIndex] = useState<number>(0);
 
-    useEffect(() => {
-        if(cafes.length === 1){
-            setCurrentCafeIndex(0);
+  useEffect(function resetIndexOnListChanged() {
+    setCafeIndex(0);
+  }, [cafes]);
+
+  const currentCafe = cafes[Math.min(cafeIndex, cafes.length - 1)];
+
+  /* memoize the swiping panel to ensure that DOM element executing the animation is preserved */
+  const memoizedSwipeablePanel = useMemo(() => {
+    const key = `TypeCafe[]#id=${cafes[0].id}`;
+
+    return (
+      <SwipeablePanel
+        key={key /* ensure that SwipeablePanel is not recycled when rendering different set of cafes */}
+        axis={Axis.horizontal}
+        onPageChanged={setCafeIndex}
+        loop
+      >
+        {cafes.map((cafe) => (
+          <StyledCarouselImage key={cafe.id}>
+            <CarouselMainImage cafe={cafe} />
+          </StyledCarouselImage>
+        ))}
+      </SwipeablePanel>
+    );
+  }, [cafes]);
+
+  return (
+    <div className="carousel-container">
+      <div className="cafe-preview-info">
+        <h4>{currentCafe?.name}</h4>
+        <span className="cafe-preview-info-list">OPEN {currentCafe?.metadata?.hour}</span>
+        <span className="cafe-preview-info-by">{currentCafe.metadata?.creator || 'jyuunnii'} 님이 올려주신 {currentCafe?.name}</span>
+      </div>
+      <div className="cafe-preview-carousel-wrapper">
+        {
+          cafes && cafes.length > 1 && memoizedSwipeablePanel
         }
-    }, [cafes.length, setCurrentCafeIndex])
-
-    if(cafes.length === 1){
-        return  <CarouselMainImage cafe={cafes[0]}/>
-    }
-
-    return(
-        <CarouselHorizontal title="Carousel" setCurrentIndex={setCurrentCafeIndex} ref={ref}>
-        {cafes?.map((cafe) => {
-            return(
-                <StyledCarouselImage key={cafe.id}>
-                    <CarouselMainImage cafe={cafe}/>
-                </StyledCarouselImage>
-        )})}
-        </CarouselHorizontal>
-    )
+        {
+          cafes && cafes.length === 1 && (
+            <CarouselMainImage cafe={cafes[0]} />
+          )
+        }
+      </div>
+      <StyledRowFlex className="cafe-preview-websearch">
+        <span onClick={() => openSearch((currentCafe?.name)+" "+currentCafe.place.name, "Naver")}>네이버 바로가기</span>
+        <span onClick={() => openSearch((currentCafe?.name), "Instagram")}>인스타그램 바로가기</span>       
+      </StyledRowFlex>
+    </div>
+  );
 }
 
 export default CafeByPlace;
--- app/src/components/pages/Intro/index.tsx
+++ app/src/components/pages/Intro/index.tsx
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { openSearch } from '../../../utils/function';
-import { StyledColumnFlex, StyledMainScale, StyledRowFlex } from '../../../utils/styled';
+import { StyledColumnFlex, StyledMainScale } from '../../../utils/styled';
 import { TypeCafe, TypePlace } from '../../../utils/type';
 import { getCafeListByPlace, getPlaceList } from '../../api';
 import PlaceSlide from '../../others/PlaceSlide';
 import CafeByPlace from '../../others/CafeByPlace';
-import './index.css';
 
 const Intro = () => {
     const [places, setPlaces] = useState<TypePlace[]>([]);
     const [currentPlaceIndex, setCurrentPlaceIndex] = useState<number>(0);
     const [cafes, setCafes] = useState<TypeCafe[] | null>(null)
-    const [currentCafeIndex, setCurrentCafeIndex] = useState<number>(0);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -41,18 +38,7 @@ const Intro = () => {
         <StyledMainScale>
             {cafes && 
                 <StyledColumnFlex className="intro">
-                    <div className="carousel-container">
-                        <div className="cafe-preview-info">
-                            <h4>{cafes[currentCafeIndex]?.name}</h4>
-                            <span className="cafe-preview-info-list">OPEN {cafes[currentCafeIndex]?.metadata?.hour}</span>
-                            <span className="cafe-preview-info-by">{cafes[currentCafeIndex].metadata?.creator || 'jyuunnii'} 님이 올려주신 {cafes[currentCafeIndex]?.name}</span>
-                        </div>
-                        <CafeByPlace cafes={cafes} setCurrentCafeIndex={setCurrentCafeIndex}/>
-                        <StyledRowFlex className="cafe-preview-websearch">
-                            <span onClick={() => openSearch((cafes[currentCafeIndex]?.name)+" "+cafes[currentCafeIndex].place.name, "Naver")}>네이버 바로가기</span>
-                            <span onClick={() => openSearch((cafes[currentCafeIndex]?.name), "Instagram")}>인스타그램 바로가기</span>       
-                        </StyledRowFlex>
-                    </div>
+                    <CafeByPlace cafes={cafes} />
                     <PlaceSlide places={places} currentPlaceIndex={currentPlaceIndex} setCurrentPlaceIndex={setCurrentPlaceIndex}/>
                 </StyledColumnFlex>
             }  
