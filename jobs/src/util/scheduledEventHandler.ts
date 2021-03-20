c4fcf5fd1e1a271833fbb5970df6e7031e4690f3
inhibitor <inhibitor inhibitor@kaist.ac.kr>
2021-03-20 22:10:00 +0900

    feat(job): move scheduled event handler to job module (#85)

--- jobs/src/util/scheduledEventHandler.ts
+++ jobs/src/util/scheduledEventHandler.ts
@@ -1,12 +1,43 @@
+import 'pg';
+
+import { readFile } from 'fs';
 import { Exception } from '@coffee-hmm/common';
-import { Connection } from 'typeorm';
-import { logLevel } from '.';
-import { connect } from '../middlewares/db';
+import path from 'path';
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
+import { env, logLevel } from '.';
+import entities from '../entities';
 import {
   ScheduledEventContext,
   ScheduledEventHandler,
-} from '../types/scheduler';
+} from '../types/scheduledEvent';
 import Logger from './logger';
+
+const readOrmConfig = (): Promise<ConnectionOptions> =>
+  new Promise<AnyJson>((resolve, reject) => {
+    if (process.env.TEST_ORMCONFIG) {
+      resolve(JSON.parse(process.env.TEST_ORMCONFIG));
+      return;
+    }
+
+    readFile(
+      path.resolve(process.cwd(), env('ORMCONFIG_FILE')),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const config = JSON.parse(data);
+          resolve(config);
+        }
+      }
+    );
+  }).then((config) => (config as unknown) as ConnectionOptions);
+
+export const connect = async () =>
+  createConnection({
+    ...(await readOrmConfig()),
+    entities,
+  });
 
 const scheduledEventHandler = (handler: ScheduledEventHandler) => () => {
   let connection: Connection | null = null;
