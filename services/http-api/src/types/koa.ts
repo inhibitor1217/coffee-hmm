e6719dbfc2ef5ac61bb9f5425255f352a7d3c121
dugi <dugi@channel.io>
2021-04-14 13:06:58 +0900

    [NO-MERGE] test(api): decrease type coverage by setting types to any

--- services/http-api/src/types/koa.ts
+++ services/http-api/src/types/koa.ts
@@ -1,25 +1,9 @@
-import { IamPolicy, OperationSchema } from '@coffee-hmm/common';
-import * as Koa from 'koa';
-import { Schema } from 'joi';
-import { Connection } from 'typeorm';
-import app from '../app';
-import Logger from '../util/logger';
-import { DataLoaders } from '../middlewares/db';
+export type KoaContextState = any;
 
-export interface KoaContextState {
-  logger: Logger;
-  connection(): Promise<Connection>;
-  loaders: DataLoaders;
-  uid?: string;
-  policy?: IamPolicy;
-}
+export type VariablesMap = any;
 
-export type VariablesMap = { [key: string]: string };
-
-type TransformedVariable = string | number | boolean;
-export type TransformedVariablesMap = {
-  [key: string]: TransformedVariable | TransformedVariable[] | undefined | null;
-};
+type TransformedVariable = any;
+export type TransformedVariablesMap = any
 
 export enum TransformedSchemaTypes {
   integer = 'integer',
@@ -27,55 +11,26 @@ export enum TransformedSchemaTypes {
   boolean = 'boolean',
 }
 
-export type TransformedFields = { key: string; type: TransformedSchemaTypes }[];
+export type TransformedFields = any;
 
 export type TransformedKoaContext<
   ParamsT extends TransformedVariablesMap = TransformedVariablesMap,
   QueryT extends TransformedVariablesMap = TransformedVariablesMap,
   BodyT = AnyJson
-> = {
-  params: ParamsT;
-  query: QueryT;
-  request: {
-    body?: BodyT;
-  };
-  state: KoaContextState;
-  status?: number;
-  body?: AnyJson;
-};
+> = any;
 
-export type KoaRouteHandler = (
-  ctx: Koa.ParameterizedContext<KoaContextState>
-) => Promise<void> | void;
+export type KoaRouteHandler = any
 
 export type TransformedKoaRouteHandler<
   ParamsT extends TransformedVariablesMap = TransformedVariablesMap,
   QueryT extends TransformedVariablesMap = TransformedVariablesMap,
   BodyT = AnyJson
-> = (
-  ctx: TransformedKoaContext<ParamsT, QueryT, BodyT>
-) => Promise<void> | void;
+> = any;
 
-export interface KoaRouteHandlerOptions<
+export type KoaRouteHandlerOptions<
   ParamsT extends TransformedVariablesMap = TransformedVariablesMap,
   QueryT extends TransformedVariablesMap = TransformedVariablesMap,
   BodyT = AnyJson
-> {
-  schema?: {
-    params?: Schema;
-    query?: Schema;
-    body?: Schema;
-  };
-  transform?: {
-    params?: TransformedFields;
-    query?: TransformedFields;
-  };
-  requiredRules?:
-    | OperationSchema
-    | OperationSchema[]
-    | ((
-        ctx: TransformedKoaContext<ParamsT, QueryT, BodyT>
-      ) => OperationSchema | OperationSchema[]);
-}
+> = any;
 
-export type KoaServer = ReturnType<typeof app.listen>;
+export type KoaServer = any;
