9dc22377abeaff1f437b5c828622242f9f1fc6a9
dugi <dugi@channel.io>
2021-08-15 19:29:14 +0900

    fix: ensure app stage is correctly applied at compile time

--- mobile_app/lib/util/environment.dart
+++ mobile_app/lib/util/environment.dart
@@ -4,6 +4,18 @@ class Environment {
   static const appName =
       String.fromEnvironment('APP_NAME', defaultValue: 'coffee hmm');
 
-  static final appStage = AppStageUtil.parse(
-      String.fromEnvironment('APP_STAGE', defaultValue: 'dev'));
+  static const _rawAppStage =
+      String.fromEnvironment('APP_STAGE', defaultValue: 'dev');
+
+  static final appStage = AppStageUtil.parse(_rawAppStage);
+
+  static String get apiBaseUrl {
+    switch (appStage) {
+      case AppStage.development:
+      case AppStage.beta:
+        return 'https://beta.api.coffee-hmm.inhibitor.io';
+      case AppStage.release:
+        return 'https://release.api.coffee-hmm.inhibitor.io';
+    }
+  }
 }
