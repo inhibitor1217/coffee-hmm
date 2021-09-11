ec9784697889158a5aef893a27ce32c1869debdc
dugi <dugi@channel.io>
2021-09-11 14:18:45 +0900

    feat: implement new cafe image progres indicator

--- mobile_app/lib/cafe_image.dart
+++ mobile_app/lib/cafe_image.dart
@@ -22,20 +22,75 @@ class CafeImage extends StatelessWidget {
       child: CachedNetworkImage(
         imageUrl: '$path$group$uri$query',
         fit: BoxFit.cover,
-        progressIndicatorBuilder: (context, url, downloadProgress) {
-          return Center(
-              child: SizedBox(
-                  width: size * 0.1,
-                  height: size * 0.1,
-                  child: CircularProgressIndicator(
-                      color: Colors.black12,
-                      strokeWidth: size > 319 ? 5 : 2,
-                      value: downloadProgress.progress)));
-        },
+        progressIndicatorBuilder: (context, url, downloadProgress) => Center(
+            child: _CafeImageProgressIndicator(
+                key: ValueKey(image.id),
+                contentSize: size,
+                progress: downloadProgress.progress ?? 0.0)),
         errorWidget: (context, error, stackTrace) {
           return Center(child: Text('no image'));
         },
       ),
     );
+  }
+}
+
+class _CafeImageProgressIndicator extends StatelessWidget {
+  static const _color = Colors.black12;
+
+  final double contentSize;
+  final double progress;
+  _CafeImageProgressIndicator(
+      {required Key key, required this.contentSize, required this.progress})
+      : super(key: key);
+
+  double get _displayedProgress => progress.clamp(0.1, 1.0);
+  double get _desiredSize => contentSize * 0.25;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Icon(Icons.image,
+          color: _color, size: _roundToMultipleOfFour(_desiredSize * 0.75)),
+      SizedBox(height: _roundToMultipleOfFour(_desiredSize * 0.05)),
+      _progressBar(context)
+    ]);
+  }
+
+  final _progressBarBorderRadius = BorderRadius.circular(4.0);
+
+  double get _progressBarWidth => _roundToMultipleOfFour(_desiredSize);
+  double get _progressBarHeight =>
+      _roundToMultipleOfFour(_progressBarWidth * 0.125)
+          .clamp(4.0, double.infinity);
+
+  Widget _progressBar(BuildContext context) {
+    return Container(
+        width: _progressBarWidth,
+        height: _progressBarHeight,
+        padding: const EdgeInsets.all(2.0),
+        decoration: BoxDecoration(
+            color: _color, borderRadius: _progressBarBorderRadius),
+        child: LayoutBuilder(
+            builder: (context, constraints) => Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    _animatedProgress(context, maxSize: constraints.maxWidth),
+                    Spacer()
+                  ],
+                )));
+  }
+
+  Widget _animatedProgress(BuildContext context, {required double maxSize}) {
+    return AnimatedContainer(
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: _progressBarBorderRadius),
+        duration: const Duration(milliseconds: 150),
+        curve: Curves.ease,
+        constraints: BoxConstraints(maxWidth: maxSize * _displayedProgress));
+  }
+
+  double _roundToMultipleOfFour(double value) {
+    return (value * 0.25).roundToDouble() * 4.0;
   }
 }
