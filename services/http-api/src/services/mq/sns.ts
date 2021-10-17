955b86704912146f5636c333a3b117cf9fb061e4
dugi <dugi@channel.io>
2021-10-17 11:12:50 +0900

    feat(api): implement SnsMessageQueue service

--- services/http-api/src/services/mq/sns.ts
+++ services/http-api/src/services/mq/sns.ts
@@ -1,11 +1,52 @@
+import { SNS } from 'aws-sdk';
+import { getSecretStorage } from '../secret';
 import type MessageQueue from '.';
 
 export default class SnsMessageQueue implements MessageQueue {
+  private readonly sns: SNS;
+
+  private static readonly DEFAULT_MESSAGE_GROUP = 'defaultMessageGroup';
+
+  constructor() {
+    this.sns = new SNS();
+  }
+
   async publish(
     topic: string,
     name: string,
     content: AnyJson
   ): Promise<string> {
-    return Promise.reject(new Error('not implemented'));
+    const params: SNS.Types.PublishInput = {
+      TopicArn: await SnsMessageQueue.getTopicResourceId(topic),
+      Message: name,
+      MessageAttributes: {
+        content: SnsMessageQueue.mapJson(content),
+      },
+      MessageGroupId: SnsMessageQueue.DEFAULT_MESSAGE_GROUP,
+    };
+
+    return new Promise<string>((resolve, reject) => {
+      this.sns.publish(params, (err, { MessageId: messageId }) => {
+        if (err) reject(err);
+        else if (!messageId)
+          reject(new TypeError('AWS SNS publish returned an empty messageId'));
+        else resolve(messageId);
+      });
+    });
+  }
+
+  /**
+   * NOTE: should implement this with smarter mechanism
+   * rather than stringifying whole content, `MessageAttributeMap` could be generated here
+   */
+  private static mapJson(json: AnyJson): SNS.Types.MessageAttributeValue {
+    return {
+      DataType: 'String',
+      StringValue: JSON.stringify(json),
+    };
+  }
+
+  private static getTopicResourceId(topic: string): Promise<string> {
+    return getSecretStorage().getGlobal(`topic.${topic}`);
   }
 }
