d1cb462c9597400e51d6900f2c58391863c75364
dugi <dugi@channel.io>
2021-10-23 16:54:41 +0900

    feat: implement SsmParameterService, SlackService, and sending cafe creation message

---
+++ jobs/src/services/ssm/index.ts
@@ -0,0 +1,62 @@
+import { SSM } from 'aws-sdk';
+import { env } from '../../util';
+
+function getValue(
+  ssmParameterList: SSM.ParameterList | undefined,
+  key: string
+): string | undefined {
+  return ssmParameterList?.find(({ Name }) => Name?.endsWith(key) ?? false)
+    ?.Value;
+}
+
+class SsmParameterStorage {
+  private ssm: SSM;
+
+  constructor() {
+    this.ssm = new SSM();
+  }
+
+  async get(key: string): Promise<string> {
+    const parameters = await this.readParameters(
+      `/${env('APP_NAME')}/${env('APP_STAGE')}`
+    );
+
+    const value = getValue(parameters, key);
+
+    if (!value) {
+      throw Error(
+        `SSM parameter of key ${key} is not set in application context`
+      );
+    }
+
+    return value;
+  }
+
+  async getGlobal(key: string): Promise<string> {
+    const parameters = await this.readParameters(
+      `/${env('SECRET_GLOBAL_VARIABLE_PREFIX')}/${env('APP_STAGE')}`
+    );
+
+    const value = getValue(parameters, key);
+
+    if (!value) {
+      throw Error(`SSM parameter of key ${key} is not set in global context`);
+    }
+
+    return value;
+  }
+
+  private readParameters(
+    pathPrefix: string
+  ): Promise<SSM.ParameterList | undefined> {
+    return this.ssm
+      .getParametersByPath({
+        Path: pathPrefix,
+        WithDecryption: true,
+      })
+      .promise()
+      .then(({ Parameters }) => Parameters);
+  }
+}
+
+export default new SsmParameterStorage();
