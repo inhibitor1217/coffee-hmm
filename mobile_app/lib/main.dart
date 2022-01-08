42c295f73e5d18ae7943add0421934c6ed0525e8
jyuunnii <yuunnii315@gmail.com>
2022-01-09 00:48:58 +0900

    feat: ios rotate disallowed

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mobile_app/router/app_route_information_parser.dart';
 import 'package:mobile_app/router/app_router_delegate.dart';
 
@@ -20,6 +21,10 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp.router(
         title: '카페 추천은, 커피흠',
         theme: new ThemeData(
