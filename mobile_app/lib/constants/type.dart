3a2013c24209d63ec9550ca9a80d7743a48f568f
jyuunnii <yuunnii315@gmail.com>
2022-01-31 11:36:41 +0900

    fix: CafeDetailSectionTitle optional 필드 required 변경 및 메타데이터 모델링 수정 - mainMenus get 으로 분리

--- mobile_app/lib/constants/type.dart
+++ mobile_app/lib/constants/type.dart
@@ -162,7 +162,6 @@ class CafeMetadataModel {
   final CafeMetaHoursModel? hours;
   final CafeMetaLocationModel? location;
   final List<CafeMetaMenuModel>? menus;
-  final List<CafeMetaMenuModel>? mainMenus;
 
   CafeMetadataModel(
       {this.creator,
@@ -183,7 +182,6 @@ class CafeMetadataModel {
     final menusFromJson =  json?['menus'] ?? [];
     final List<CafeMetaMenuModel> menus = List.unmodifiable(
         menusFromJson.map((menu) => CafeMetaMenuModel.fromJson(menu)).toList());
-    final List<CafeMetaMenuModel> mainMenus = menus.where((menu) => menu.isMain ?? false ).toList();
 
     return CafeMetadataModel(
         creator: json?['creator'],
