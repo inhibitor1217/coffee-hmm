f7df30e87b1f6f8c27b0cebf93a33be6c9a3d505
jyuunnii <yuunnii315@gmail.com>
2022-02-05 15:22:45 +0900

    fix: remove enumToString

--- mobile_app/lib/util/type_transformer.dart
+++ mobile_app/lib/util/type_transformer.dart
@@ -1,16 +1,4 @@
 extension EnumTransform on String {
-  String enumToString<T>(List<T> list, T value){
-    final defaultValue = this;
-
-    if(value == null || (list.isEmpty)) return defaultValue;
-
-    final T? target = list.firstWhere((_enum) => _enum.toString() == value.toString());
-
-    if(target == null) return defaultValue;
-
-    return target.toString().split('.').last;
-  }
-
   T stringToEnum<T>(List<T> list, T defaultValue){
     return list.firstWhere((_enum) => _enum.toString().split('.').last == this,
         orElse: () => defaultValue);
