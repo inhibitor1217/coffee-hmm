2af6033fd2d7cb646493efc5f2908cdce2fb6a50
jyuunnii <yuunnii315@gmail.com>
2022-03-13 23:40:57 +0900

    fix: restore unrelated code modification

--- mobile_app/lib/view/common/button/custom_icon_button.dart
+++ mobile_app/lib/view/common/button/custom_icon_button.dart
@@ -42,7 +42,7 @@ class CustomIconButton extends StatelessWidget {
 }
 class IconButtonContent {
   final String text;
-  final Icon icon;
+  final IconData icon;
 
   IconButtonContent({required this.text, required this.icon});
 }
