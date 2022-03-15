085a1e9396158a303e5720d41da82ed87ecc3813
jyuunnii <yuunnii315@gmail.com>
2022-03-15 20:57:09 +0900

    feat: create common Feed class and apply it

--- mobile_app/lib/view/saved/saved_body_content.dart
+++ mobile_app/lib/view/saved/saved_body_content.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_app/constants/color.dart';
 import 'package:mobile_app/router/mixins/enter_cafe_detail_mixin.dart';
 import 'package:mobile_app/storage/cafe.dart';
+import 'package:mobile_app/view/common/feed.dart';
 import 'package:mobile_app/view/saved/saved_element.dart';
 
 class SavedBodyContent extends StatefulWidget {
@@ -10,11 +11,22 @@ class SavedBodyContent extends StatefulWidget {
 }
 
 class _SavedBodyContentState extends State<SavedBodyContent> with SavedCafe, EnterCafeDetailMixin{
+  final ScrollController _scrollController = ScrollController();
   List<String> _savedCafeIds = [];
 
   @override
   void initState() {
     super.initState();
+    _updateSavedCafeIds();
+  }
+  @override
+  void didUpdateWidget(covariant SavedBodyContent oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _savedCafeIds.clear();
+    _updateSavedCafeIds();
+  }
+
+  void _updateSavedCafeIds() {
     getAllSavedCafeIds().then((data) => {
       setState(() {
         _savedCafeIds = data.cast<String>();
@@ -24,13 +36,17 @@ class _SavedBodyContentState extends State<SavedBodyContent> with SavedCafe, Ent
 
   @override
   Widget build(BuildContext context){
-    // FIXME 추후 수정
     if(_savedCafeIds.length > 0) {
-      return Column(
-        children: [
-          SavedElement(cafeId: _savedCafeIds[0]),
-          SavedElement(cafeId: _savedCafeIds[1])
-        ],
+      return Feed(
+        scrollController: _scrollController,
+        children: _savedCafeIds.map((id) =>  GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: enterDetail(id),
+          child: Container(
+            padding: EdgeInsets.only(right: _savedCafeIds.indexOf(id)%3 == 2 ? 0 : 1, bottom: 1),
+            child: SavedElement(cafeId: id),
+          ),
+        )).toList(),
       );
     }else {
       Text('empty');
